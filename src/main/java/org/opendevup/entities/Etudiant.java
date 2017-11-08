81fc6bfc44a703d0fdaa196ed0eee94353cbd794
imed hammami <hamami.imed@gmail.com>
2017-11-08 14:53:59 +0100

    upload file

--- src/main/java/org/opendevup/entities/Etudiant.java
+++ src/main/java/org/opendevup/entities/Etudiant.java
@@ -7,60 +7,67 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 public class Etudiant  implements Serializable{
 	@Id
 	@GeneratedValue
-private Long id;
+	private Long id;
 	@Column (name="NOM", length=40)
-private String nom;
-@DateTimeFormat(pattern="yyyy-MM-dd")	
-private Date dateNaissance;
-private String email;
-//private byte[] photo;
-private String photo;
-public Long getId() {
-	return id;
-}
-public void setId(Long id) {
-	this.id = id;
-}
-public String getNom() {
-	return nom;
-}
-public void setNom(String nom) {
-	this.nom = nom;
-}
-public Date getDateNaissance() {
-	return dateNaissance;
-}
-public void setDateNaissance(Date dateNaissance) {
-	this.dateNaissance = dateNaissance;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getPhoto() {
-	return photo;
-}
-public void setPhoto(String photo) {
-	this.photo = photo;
-}
-public Etudiant(String nom, Date dateNaissance, String email, String photo) {
-	super();
-	this.nom = nom;
-	this.dateNaissance = dateNaissance;
-	this.email = email;
-	this.photo = photo;
-}
-public Etudiant() {
-	super();
-	// TODO Auto-generated constructor stub
-}
+	@NotEmpty
+	@Size(min=3,max=30)
+	private String nom;
+	@DateTimeFormat(pattern="yyyy-MM-dd")	
+	private Date dateNaissance;
+	@NotEmpty
+	@Email
+	private String email;
+	//private byte[] photo;
+	private String photo;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public Etudiant(String nom, Date dateNaissance, String email, String photo) {
+		super();
+		this.nom = nom;
+		this.dateNaissance = dateNaissance;
+		this.email = email;
+		this.photo = photo;
+	}
+	public Etudiant() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/main/java/org/opendevup/web/EtudiantController.java
+++ src/main/java/org/opendevup/web/EtudiantController.java
@@ -1,6 +1,10 @@
 package org.opendevup.web;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.opendevup.dao.EtudiantRepository;
 import org.opendevup.entities.Etudiant;
@@ -9,8 +13,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller  
 @RequestMapping(value="/Etudiant")
@@ -45,7 +51,18 @@ public class EtudiantController {
 	}
 	
 	@RequestMapping(value="/SaveEtudiant",method=RequestMethod.POST)
-	public String save(Etudiant et){
+	public String save(@Valid Etudiant et, BindingResult bindingResult,@RequestParam (name="picture") MultipartFile file) throws IllegalStateException, IOException{
+		if (bindingResult.hasErrors())
+		{ 
+			return "formEtudiant";
+
+		}
+		if (!(file.isEmpty()))
+		{
+			et.setPhoto(file.getOriginalFilename());
+			file.transferTo(new File(System.getProperty("user.home")+"/sco_file"));
+			//file.transferTo(new File(System.getProperty("user.home")+"/sco_file"));
+		}
 		etudiantRepository.save(et);
 		return "redirect:Index";
 	}
